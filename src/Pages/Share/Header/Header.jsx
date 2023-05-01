f524e8509b35ea78717c473eb69da3c1d10766b8
Md-Rijwan-Jannat <rijwanjannat36@gmail.com>
2023-05-01 23:44:02 +0600

    create a header

--- src/Pages/Share/Header/Header.jsx
+++ src/Pages/Share/Header/Header.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is a header</h1>
+        <div className="navbar bg-base-100 border">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </label>
+            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+              <li><Link to={'/'}>Home</Link></li>
+              <li><Link to={'/blog'}>Blog</Link></li>
+            </ul>
+          </div>
+          <img className='w-28' src="https://img.freepik.com/free-vector/detailed-chef-logo-template_23-2148987940.jpg?w=740&t=st=1682962415~exp=1682963015~hmac=bcf3e0201b3e5a61add8cf12bc7f7b94cac6c432209742ba6275a3aa3d5e0ce8" alt="" />
+          <img className='w-36' src="https://findvectorlogo.com/wp-content/uploads/2022/02/camp-chef-vector-logo-2022.png" alt="" />
         </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li className='font-bold'><Link to={'/'}>Home</Link></li>
+            <li className='font-bold'><Link to={'/blog'}>Blog</Link></li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <Link to={'/login'} className="btn bg-red-700 border-none hover:bg-orange-400">Login</Link >
+        </div>
+      </div>
     );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import '../src/Components/index.css'
 import {
   RouterProvider,
 } from "react-router-dom";
