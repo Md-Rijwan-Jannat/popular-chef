7886256f8aae6b2c38c8bef8896685f3180a9689
Md-Rijwan-Jannat <rijwanjannat36@gmail.com>
2023-05-04 00:32:10 +0600

    modefied more

--- src/Components/Banner/RecipeBanner/RecipeBanner.jsx
+++ src/Components/Banner/RecipeBanner/RecipeBanner.jsx
@@ -11,7 +11,7 @@ const RecipeBanner = ({ recipeDetails }) => {
                 <figure><img className='rounded-xl w-full h-full' src={chef_picture} alt="Album" /></figure>
                 <div className="card-body">
                     <h2 className="card-title">{chef_name}</h2>
-                    <p>{bio}</p>
+                    <p><small>{bio}</small></p>
                     <p style={{ fontFamily: 'serif' }} className='text-2xl font-medium'>Year of experience: <span className='font-semibold text-orange-500 '>{year_of_experience}</span></p>
                     <p style={{ fontFamily: 'serif' }} className='text-lg font-medium'>Total recipes:  <span className='font-semibold text-orange-500'>{recipes_number}</span></p>
                     <div style={{ fontFamily: 'cursive' }} className='text-xl flex justify-between p-2 gap-2 items-center rounded-lg shadow-lg text-red-600 '>
@@ -23,13 +23,17 @@ const RecipeBanner = ({ recipeDetails }) => {
                     </div>
                 </div>
             </div>
-            <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3'>
-                {
-                    recipes.map(rec => <Recipe
-                        key={rec.id}
-                        rec={rec}
-                    ></Recipe>)
-                }
+            <div>
+                <h2 className='text-3xl text-orange-500 text-center my-10 font-semibold'>Name of <span className='text-red-600 font-bold'>recipe</span></h2>
+                <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3'>
+
+                    {
+                        recipes.map(rec => <Recipe
+                            key={rec.id}
+                            rec={rec}
+                        ></Recipe>)
+                    }
+                </div>
             </div>
         </div>
     );
